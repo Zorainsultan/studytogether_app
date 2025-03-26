f8724bd73f79757c9c2c2f1ad2be54346f6a24cb
ZORAIN SULTAN <72017726+Zorainsultan@users.noreply.github.com>
2025-03-26 00:23:59 +0000

    inital bits, app name etc

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+//import flutter's material design widgets
+import 'package:flutter/material.dart';
+
+//import the login page from pages folder
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
